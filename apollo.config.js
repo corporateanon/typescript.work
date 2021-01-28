08a4ec30bc303d378ab5a315d0e057a813a3ffab
corporateanon@gmail.com <corporateanon>
2021-01-28 08:40:26 +0200

    init

---
+++ apollo.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    client: {
+        includes: ['./{pages,lib,components,models}/**/*.{ts,tsx,graphql}'],
+        // service: {
+        //     name: 'GitHub',
+        //     localSchemaFile: './github.schema.graphql',
+        // },
+    },
+};
